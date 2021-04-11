1be82cccd6b752d4466389144fd7d7f5208d7a7b
Mahmoud Elsharawy <mahmoudelsharawy1797@yahoo.com>
2021-04-12 00:17:09 +0200

    getting error solved

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,19 +1,37 @@
+import 'package:clima/services/location.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+
 class LoadingScreen extends StatefulWidget {
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+
+  void getLocation()async{
+    Location location=Location();
+    await location.getCurrentLocation();
+    print(location.latitude);
+    print(location.longitude);
+  }
+
+  void getData()async{
+    print("first");
+    Response response = await get('https://worldtimeapi.org/api/timezone/Africa/Nairobi');
+    print("end");
+    print(response.body);
+  }
+  @override
+  void initState() {
+    getLocation();
+    getData();
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {  },
-          child: Text('Get Location'),
-        ),
-      ),
     );
   }
 }
